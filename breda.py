c17694cde0eed0a4c66dd48dc2ebaf294ab7e302
idiot <jure.koren@zemanta.com>
2014-02-12 13:50:45 +0100

    seyren to slack gateway

--- breda.py
+++ breda.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
-import json, authsettings, os, traceback
+import json, authsettings, os, traceback, urllib2, urllib, re
 
 import flask
 app = flask.Flask(__name__)
 
 import handlers
 import smartass
+
+
+def _post_message(username, channel, text, parse='none'):
+	args = dict(
+		token=authsettings.POST_TOKEN,
+		channel=channel,
+		text=text,
+		username=username,
+		pretty='1',
+		parse=parse,
+		link_names='1',
+	)
+	urllib2.urlopen('https://slack.com/api/chat.postMessage', urllib.urlencode(args))
 
 
 @app.route('/msg/', methods=['POST'])
@@ -24,6 +37,7 @@ def process_message():
 	except:
 		return json.dumps(dict(text="Oh, bummer: " + traceback.format_exc()))
 
+
 @app.route('/push/', methods=['POST'])
 def push():
 	data = json.loads(flask.request.form.get('payload', {}))
@@ -33,6 +47,42 @@ def push():
 	os.system('supervisorctl restart breda')
 	return 'OK'
 
+
+@app.route('/seyren/', methods=['POST'])
+def relay_seyren():
+	check = flask.request.json.get('check')
+	check_url = 'http://seyren.zemanta.com/#/checks/%s' % check.get('id')
+	check_name = check.get('name', '')
+	check_desc = check.get('description', '')
+	preview = flask.request.json.get('preview')
+	try:
+		chart_url = re.findall(r'img src=(.*)></img>$', preview)[0]
+	except:
+		chart_url = ''
+	alerts = flask.request.json.get('alerts', [])
+	for alert in alerts:
+		from_type = alert.get('fromType', 'unknown')
+		to_type = alert.get('toType', 'unknown')
+		value = alert.get('value', 0)
+		error = alert.get('error', 0)
+		warn = alert.get('warn', 0)
+		if to_type == 'OK':
+			emo = 'smile'
+		elif to_type == 'WARN':
+			emo = 'angry'
+		elif to_type == 'ERROR':
+			emo = 'rage'
+		else:
+			emo = 'confused'
+		_post_message('seyren', '#devops', '<%(check_url)s|%(check_name)s> changed from %(from_type)s to %(to_type)s, value was %(value)s (warn: %(warn)s, crit: %(error)s) :%(emo)s:' % vars())
+		if chart_url:
+			_post_message('seyren', '#devops', '<%s|check chart>' % chart_url)
+	return flask.Response(
+		'OK',
+		status=200,
+		mimetype='text/plain')
+
+
 @app.errorhandler(500)
 def unauthorized_500(error):
 	return flask.Response(
@@ -40,12 +90,14 @@ def unauthorized_500(error):
 		status=200,
 		mimetype='text/plain')
 
+
 @app.errorhandler(403)
 def unauthorized_403(error):
 	return flask.Response(
 		'You are not authorized to access this service.',
 		status=403,
 		mimetype='text/plain')
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=3201)
